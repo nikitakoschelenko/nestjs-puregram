e095846ea5f85f14d8ad141909208b65f7f3784a
ItzNeviKat <nikitakoschelenko@gmail.com>
2022-04-24 21:57:52 +0300

    fix: only fields of base context were valid

--- lib/decorators/params/context.decorator.ts
+++ lib/decorators/params/context.decorator.ts
@@ -4,13 +4,15 @@ import { Context as TelegramContext } from 'puregram';
 import { TelegramExecutionContext } from '../../execution-context';
 
 export const Context = createParamDecorator(
-  (data: keyof TelegramContext | never, context: ExecutionContext) => {
+  (data: keyof TelegramContext | string | never, context: ExecutionContext) => {
     const executionContext: TelegramExecutionContext =
       TelegramExecutionContext.create(context);
 
     const telegramContext: TelegramContext = executionContext.getContext();
 
-    return data ? telegramContext[data] : telegramContext;
+    return data
+      ? telegramContext[data as keyof TelegramContext]
+      : telegramContext;
   }
 );
 
