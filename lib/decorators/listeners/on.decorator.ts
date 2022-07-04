291850fd95f03752c1d4eb8b8b885f805f5260b9
Nikita Koshelenko <nikitakoschelenko@gmail.com>
2022-07-04 21:18:16 +0300

    chore(deps)

--- lib/decorators/listeners/on.decorator.ts
+++ lib/decorators/listeners/on.decorator.ts
@@ -1,11 +1,11 @@
 import { Middleware } from 'middleware-io';
 import { Context, UpdateType } from 'puregram';
-import { AllowArray, UpdateName } from 'puregram/types';
+import { MaybeArray, UpdateName } from 'puregram/types';
 
 import { ListenerHandlerType } from '../../enums/listener-handler-type.enum';
 import { createListenerDecorator } from '../../utils';
 
 export const On = <T = {}>(
   update: UpdateType | UpdateName,
-  middlewares?: AllowArray<Middleware<Context & T>>
+  middlewares?: MaybeArray<Middleware<Context & T>>
 ) => createListenerDecorator(ListenerHandlerType.ON, update, middlewares);
